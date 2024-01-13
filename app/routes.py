a13bdecf4a7fd0397514f0c7c2f6b2f31d1c6e32
Felipe Baptista <felipe.baptista06@gmail.com>
2024-01-13 03:38:43 -0300

    created dashboard style

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,4 @@
+from flask_cors import cross_origin
 from app import app
 from flask import render_template, request, jsonify
 from app.models import User
@@ -7,12 +8,12 @@ import uuid
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html'), 200
 
 
 @app.route('/register')
 def register():
-    return render_template('register.html')
+    return render_template('register.html'), 200
 
 
 @app.route('/register-user', methods=['POST'])
@@ -27,18 +28,32 @@ def create_user():
     session.add(user)
     session.commit()
 
-    return render_template('dashboard.html'), 200
+    return render_template('/'), 200
 
 
 @app.route('/auth', methods=['POST'])
+@cross_origin(origin='*', headers=['Content-Type'])
 def authenticate():
-    email = request.form.get('email')
-    password = request.form.get('password')
+    try:
+        req_auth = request.json
+        email = req_auth.get('email')
+        password = req_auth.get('password')
 
-    user = session.query(User).filter_by(
-        email=email, password=password).first()
+        user = session.query(User).filter_by(
+            email=email, password=password).first()
 
-    if user:
-        return render_template('dashboard.html'), 200
-    else:
-        return render_template('error.html'), 401
+        print(req_auth)
+        # return True
+
+        if user:
+            return jsonify({'authenticated': True, 'user_id': user.id})
+        else:
+            return jsonify({'authenticated': False}), 401
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
+@app.route('/dashboard', methods=['GET'])
+def dashboard():
+    users = session.query(User).all()
+    return render_template('dashboard.html', users=users)
